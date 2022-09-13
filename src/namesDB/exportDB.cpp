c7fcacb2f2465ce65f6fdca5fa3a591d598ffbee
Max Kofler <kofler.max.dev@gmail.com>
2022-09-13 10:58:14 +0200

    Added additional size_t in db binary file for amount of entries stored

--- src/namesDB/exportDB.cpp
+++ src/namesDB/exportDB.cpp
@@ -9,5 +9,6 @@ void NamesDB::exportDB(std::ostream& outStream){
 	DEBUG_EX("NamesDB::exportDB()");
 
 	outStream.write((char*)&_bytesUsed, sizeof(size_t));
+	outStream.write((char*)&_count_entries, sizeof(size_t));
 	outStream.write((char*)_entries, _bytesUsed);
 }
--- src/namesDB/importDB.cpp
+++ src/namesDB/importDB.cpp
@@ -22,8 +22,18 @@ bool NamesDB::importDB(std::istream& instream){
 		return false;
 	}
 
-	size_t block_to_allocate = (_bytesUsed / _blockSize) + 1;
-	expand(block_to_allocate);
+	instream.read((char*)&_count_entries, sizeof(size_t));
+	LOGMEM("[NamesDB][importDB] Importing " + std::to_string(_count_entries) + " entries to \"" + _title + "\"...");
+
+	if (!instream){
+		LOGE(	"[NamesDB][importDB] Failed to import stream contents to \"" + 
+				_title + "\": EOF in count header, read bytes: " + std::to_string(instream.gcount()));
+		clean();
+		return false;
+	}
+
+	_bytesAllocated = _bytesUsed;
+	_entries = new uint8_t[_bytesAllocated];
 
 	instream.read((char*)_entries, _bytesUsed);
 	if (!instream){
