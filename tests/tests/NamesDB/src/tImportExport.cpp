5b0adf04aa5b4918c5a9aa3bfa3c8d346ba25a2a
Max Kofler <kofler.max.dev@gmail.com>
2022-11-11 22:41:46 +0100

    Added testing for correct import and export

--- tests/tests/NamesDB/src/tImportExport.cpp
+++ tests/tests/NamesDB/src/tImportExport.cpp
@@ -2,35 +2,46 @@
 
 #include <fstream>
 
-TEST(NamesDB, export){
+TEST(NamesDB, import_export){
 	FUN();
 
-	std::string testString = "Test";
-	std::string nullString = "null";
+	std::string string1 = "string1";
+	std::string string2 = "string2";
+	std::string string3 = "string3äü?!'*#";
 
 	NamesDB<std::string> db;
-	db.add("db", testString);
-	db.add("nullString", nullString);
+	db.add(string1, string1);
+	db.add(string2, string2);
+	db.add(string3, string3);
 
-	std::ofstream outFile;
-	db.add("äueräerüafö!ßdftestString", testString);
-	outFile.open("db.bin", std::ios::binary | std::ios::out);
 
-	db.exportDB(outFile);
+	{	//Export
+		std::ofstream outFile;
+		outFile.open("db.bin", std::ios::binary | std::ios::out);
+		db.exportDB(outFile);
 
-	outFile.flush();
-	outFile.close();
+		outFile.flush();
+		outFile.close();
+	}
 
 	NamesDB<std::string> newDB("newDB");
-	std::ifstream inFile;
-	inFile.open("db.bin", std::ios::binary | std::ios::in);
-	if (!inFile.is_open())
-		FAIL() << "Failed to open infile!";
+	
 
-	inFile.clear();
+	{	//Import
+		std::ifstream inFile;
+		inFile.open("db.bin", std::ios::binary | std::ios::in);
+		if (!inFile.is_open())
+			FAIL() << "Failed to open infile!";
+		inFile.clear();
 
-	ASSERT_TRUE(newDB.importDB(inFile));
+		ASSERT_TRUE(newDB.importDB(inFile));
+	}
 
-	ASSERT_EQ(testString, *newDB.getEntry(0)) << "Import and export did not work (Changed values)";
-	ASSERT_EQ(nullString, *newDB.getEntry(1)) << "Import and export did not work (Changed values)";
+	ASSERT_EQ(string1, *newDB.getEntry(0)) << "Import and export did not work (Changed values)";
+	ASSERT_EQ(string2, *newDB.getEntry(1)) << "Import and export did not work (Changed values)";
+	ASSERT_EQ(string3, *newDB.getEntry(2)) << "Import and export did not work (Changed values)";
+
+	ASSERT_EQ(string1, *newDB.searchFirst(string1, true).data) << "Import and export did not work (Changed values in search)";
+	ASSERT_EQ(string2, *newDB.searchFirst(string2, true).data) << "Import and export did not work (Changed values in search)";
+	ASSERT_EQ(string3, *newDB.searchFirst(string3, true).data) << "Import and export did not work (Changed values in search)";
 }
