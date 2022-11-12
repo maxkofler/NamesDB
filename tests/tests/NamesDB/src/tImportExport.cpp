b9f5b1645a2e8656e8ce3dfbedd39812d9127f72
Max Kofler <kofler.max.dev@gmail.com>
2022-11-12 11:10:46 +0100

    Fixed crashing problems in tImportExport

--- tests/tests/NamesDB/src/tImportExport.cpp
+++ tests/tests/NamesDB/src/tImportExport.cpp
@@ -41,7 +41,15 @@ TEST(NamesDB, import_export){
 	ASSERT_EQ(string2, *newDB.getEntry(1)) << "Import and export did not work (Changed values)";
 	ASSERT_EQ(string3, *newDB.getEntry(2)) << "Import and export did not work (Changed values)";
 
-	ASSERT_EQ(string1, *newDB.searchFirst(string1, true, true).data) << "Import and export did not work (Changed values in search)";
-	ASSERT_EQ(string2, *newDB.searchFirst(string2, true, true).data) << "Import and export did not work (Changed values in search)";
-	ASSERT_EQ(string3, *newDB.searchFirst(string3, true, true).data) << "Import and export did not work (Changed values in search)";
+	namesDB_searchRes<std::string> res1 = newDB.searchFirst(string1, true, true);
+	namesDB_searchRes<std::string> res2 = newDB.searchFirst(string2, true, true);
+	namesDB_searchRes<std::string> res3 = newDB.searchFirst(string3, true, true);
+
+	ASSERT_EQ(0, res1.code) << "Import and export did not work (Search resulted in error)";
+	ASSERT_EQ(0, res2.code) << "Import and export did not work (Search resulted in error)";
+	ASSERT_EQ(0, res3.code) << "Import and export did not work (Search resulted in error)";
+
+	ASSERT_EQ(string1, *res1.data) << "Import and export did not work (Changed values in search)";
+	ASSERT_EQ(string2, *res2.data) << "Import and export did not work (Changed values in search)";
+	ASSERT_EQ(string3, *res3.data) << "Import and export did not work (Changed values in search)";
 }
